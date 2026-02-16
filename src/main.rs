0cbdcb9fcf01a1aed9d9f3266e0e3173dc0dcf6d
kainosaur <152326772+kainosaur@users.noreply.github.com>
2026-02-15 21:20:00 -0700

    thoughts and prayers

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,6 @@ use bevy::{asset::AssetMetaCheck, prelude::*};
 use bevy_yarnspinner::prelude::*;
 use bevy_yarnspinner_example_dialogue_view::prelude::*;
 
-use crate::map::level::MapAssets;
-
 fn main() -> AppExit {
     App::new().add_plugins(AppPlugin).run()
 }
@@ -52,6 +50,7 @@ impl Plugin for AppPlugin {
                     ..default()
                 }),
         );
+        
         app.add_plugins((
             YarnSpinnerPlugin::with_yarn_source(YarnFileSource::file("dialogue/introduction.yarn")),
         ));
@@ -66,13 +65,13 @@ impl Plugin for AppPlugin {
         app.add_plugins((
             asset_tracking::plugin,
             audio::plugin,
+            player::plugin,
             #[cfg(feature = "dev")]
             dev_tools::plugin,
             menus::plugin,
             screens::plugin,
             theme::plugin,
             map::plugin,
-            player::plugin,
             inventory::plugin,
         ));
 
